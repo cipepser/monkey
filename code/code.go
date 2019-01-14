93f1db73c8315ba1179d6199400fd9d207dfa8a1
cipepser <respepic@gmail.com>
2019-01-14 14:32:24 +0900

    finish section

--- code/code.go
+++ code/code.go
@@ -73,6 +73,8 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 type Definition struct {
@@ -105,6 +107,8 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},
 	OpReturnValue:   {"OpReturnValue", []int{}},
 	OpReturn:        {"OpReturn", []int{}},
+	OpGetLocal:      {"OpGetLocal", []int{1}},
+	OpSetLocal:      {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -136,6 +140,9 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
@@ -151,6 +158,10 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
+
 		}
 
 		offset += width
@@ -159,6 +170,5 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 	return operands, offset
 }
 
-func ReadUint16(ins Instructions) uint16 {
-	return binary.BigEndian.Uint16(ins)
-}
+func ReadUint16(ins Instructions) uint16 { return binary.BigEndian.Uint16(ins) }
+func ReadUint8(ins Instructions) uint8   { return uint8(ins[0]) }
--- compiler/compiler.go
+++ compiler/compiler.go
@@ -268,7 +268,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
 
-		c.emit(code.OpGetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpGetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpGetLocal, symbol.Index)
+		}
 
 	case *ast.BlockStatement:
 		for _, s := range node.Statements {
@@ -284,7 +288,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return err
 		}
 		symbol := c.symbolTable.Define(node.Name.Value)
-		c.emit(code.OpSetGlobal, symbol.Index)
+		if symbol.Scope == GlobalScope {
+			c.emit(code.OpSetGlobal, symbol.Index)
+		} else {
+			c.emit(code.OpSetLocal, symbol.Index)
+		}
+
 	}
 
 	return nil
@@ -379,12 +388,15 @@ func (c *Compiler) enterScope() {
 
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 func (c *Compiler) leaveScope() code.Instructions {
 	instructions := c.currentInstructions()
 
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+
+	c.symbolTable = c.symbolTable.Outer
 
 	return instructions
 }
